80fb9d5a61f1afa3ca421d8c06f4ae2bdcdbd66d
moshebaricdo <168464139+moshebaricdo@users.noreply.github.com>
2026-03-25 23:11:52 -0400

    Update Vite configuration to change base path from '/Wl2prototype/' to './'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/Wl2prototype/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
